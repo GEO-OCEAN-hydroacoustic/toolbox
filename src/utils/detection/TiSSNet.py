a266436234fbb9293a519869ca527fabd8d6a2cb
Pierre-Yves <pierre-yves.raumer@univ-brest.fr>
2024-12-20 14:59:30 +0100

    adding TiSSNet support and a sample notebook

---
+++ src/utils/detection/TiSSNet.py
@@ -0,0 +1,53 @@
+from torch import nn
+
+class TiSSNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        modules = []
+
+        modules.append(nn.Conv2d(1, 16, kernel_size=(8, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(16, 16, kernel_size=(8, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(16, 16, kernel_size=(8, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+
+        modules.append(nn.MaxPool2d(kernel_size=(2, 1)))
+
+        modules.append(nn.Conv2d(16, 32, kernel_size=(5, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(32, 32, kernel_size=(5, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(32, 32, kernel_size=(5, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+
+        modules.append(nn.MaxPool2d(kernel_size=(4, 1)))
+
+        modules.append(nn.Conv2d(32, 64, kernel_size=(3, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(64, 64, kernel_size=(3, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(64, 64, kernel_size=(3, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+
+        modules.append(nn.MaxPool2d(kernel_size=(4, 1)))
+
+        modules.append(nn.Conv2d(64, 128, kernel_size=(2, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(128, 128, kernel_size=(2, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+        modules.append(nn.Conv2d(128, 128, kernel_size=(2, 8), padding='same'))
+        modules.append(nn.LeakyReLU(negative_slope=0.3, inplace=True))
+
+        modules.append(nn.MaxPool2d(kernel_size=(4, 1)))
+
+        modules.append(nn.Conv2d(128, 1, kernel_size=1))
+        modules.append(nn.Sigmoid())
+
+        modules.append(nn.Flatten(-3, -1))
+
+        self.layers = nn.Sequential(*modules)
+
+    def forward(self, x):
+        return self.layers(x)
