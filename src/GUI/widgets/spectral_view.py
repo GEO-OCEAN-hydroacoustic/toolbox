cleaning GUI and fixing broadcast of date slider release

src/GUI/widgets/spectral_view.py
@@ -11,6 +11,7 @@ from playsound import playsound
 from scipy import signal
 
 from GUI.widgets.mpl_canvas import MplCanvas
+from utils.physics.signal.make_spectrogram import make_spectrogram
 
 # minimal duration of the spectrograms shown, in s
 MIN_SEGMENT_DURATION_S = 30
@@ -90,9 +91,9 @@ class SpectralView(QtWidgets.QWidget):
         self.segment_length_doubleSpinBox.setValue(2 * delta_view_s)
         self.segment_length_layout.addWidget(self.segment_length_doubleSpinBox, 1, 0, 1, 1)
         # spin box triggers to update the allowed segment date range, the slider and the plot when changed
-        self.segment_length_doubleSpinBox.valueChanged.connect(self.updateDatesBounds)
-        self.segment_length_doubleSpinBox.valueChanged.connect(self.updateSegmentLengthSlider)
-        self.segment_length_doubleSpinBox.valueChanged.connect(self.updatePlot)
+        self.segment_length_doubleSpinBox.valueChanged.connect(self.update_date_bounds)
+        self.segment_length_doubleSpinBox.valueChanged.connect(self.update_segment_length_slider)
+        self.segment_length_doubleSpinBox.valueChanged.connect(self.update_plot)
 
         # segment length slider
         self.segment_length_slider = QSlider(self)
@@ -101,7 +102,7 @@ class SpectralView(QtWidgets.QWidget):
         self.segment_length_slider.setOrientation(Qt.Horizontal)
         self.segment_length_layout.addWidget(self.segment_length_slider, 2, 0, 1, 3)
         # slider trigger to update the spin box when released (and recursively the plot)
-        self.segment_length_slider.sliderReleased.connect(self.updateSegmentLengthEditor)
+        self.segment_length_slider.sliderReleased.connect(self.update_segment_length_editor)
 
         # controller of the segment date, made of a date time editor and a slider, both interconnected
         # segment date layout
@@ -118,21 +119,21 @@ class SpectralView(QtWidgets.QWidget):
         self.segment_date_dateTimeEdit = QDateTimeEdit(self)
         self.segment_date_dateTimeEdit.setInputMethodHints(Qt.ImhPreferNumbers)
         self.segment_date_dateTimeEdit.setDisplayFormat("yyyy/MM/dd hh:mm:ss")
-        self.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(self.manager.dataset_start))
+        self.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(self.manager.dataset_start))
         self.segment_date_layout.addWidget(self.segment_date_dateTimeEdit, 1, 0, 1, 1)
         # set the initital date
         if date is not None:
-            self.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(date))
+            self.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(date))
         # date time edit triggers to update the slider and the plot when changed
-        self.segment_date_dateTimeEdit.dateTimeChanged.connect(self.updateSegmentDateSlider)
-        self.segment_date_dateTimeEdit.dateTimeChanged.connect(self.updatePlot)
+        self.segment_date_dateTimeEdit.dateTimeChanged.connect(self.update_segment_date_slider)
+        self.segment_date_dateTimeEdit.dateTimeChanged.connect(self.update_plot)
 
         # segment date slider
         self.segment_date_slider = QSlider(self)
         self.segment_date_slider.setOrientation(Qt.Horizontal)
         self.segment_date_layout.addWidget(self.segment_date_slider, 2, 0, 1, 3)
         # slider trigger to update the date time edit when released (and recursively the plot)
-        self.segment_date_slider.sliderReleased.connect(self.updateSegmentDateEditor)
+        self.segment_date_slider.sliderReleased.connect(self.update_segment_date_editor)
 
         # mpl canvas containing the spectrogram
         self.mpl_layout = QtWidgets.QVBoxLayout()
@@ -143,10 +144,10 @@ class SpectralView(QtWidgets.QWidget):
 
         # initialize the different widgets
         self.layout.setStretch(1, 5)
-        self.updateDatesBounds()
-        self.updateSegmentLengthSlider()
-        self.updateSegmentDateSlider()
-        self.updatePlot()
+        self.update_date_bounds()
+        self.update_segment_length_slider()
+        self.update_segment_date_slider()
+        self.update_plot()
 
     def close_button_click(self):
         """ Close the current spectral_view.
@@ -161,39 +162,52 @@ class SpectralView(QtWidgets.QWidget):
         self.spectralViewer.unfocus_spectral_view(self)
         self.unfocus_button.setText("Unfocus" if self.focused else "Focus")
 
-    def updateSegmentLengthEditor(self):
+    def update_segment_length_editor(self):
         """ Update the segment length spin box after the slider is changed.
         :return: None.
         """
         self.segment_length_doubleSpinBox.setValue(self.segment_length_slider.value())
 
-    def updateSegmentLengthSlider(self):
+    def update_segment_length_slider(self):
         """ Update the segment length slider after the spin box is changed.
         :return: None.
         """
         self.segment_length_slider.setValue(self.segment_length_doubleSpinBox.value())
 
-    def updateSegmentDateSlider(self):
+    def update_segment_date_slider(self):
         """ Update the segment date slider after the editor is changed.
         :return: None.
         """
-        new_date = QdatetimeToDatetime(self.segment_date_dateTimeEdit.date(),
-                                      self.segment_date_dateTimeEdit.time())
+        self.segment_date_slider.sliderReleased.disconnect(self.update_segment_date_editor)
+
+        new_date = Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                         self.segment_date_dateTimeEdit.time())
         old_date = self.manager.dataset_start + datetime.timedelta(seconds=self.segment_date_slider.value())
         self.spectralViewer.notify_delta(new_date - old_date, self)
-        self.segment_date_slider.setValue((QdatetimeToDatetime(self.segment_date_dateTimeEdit.date(),
-                                                               self.segment_date_dateTimeEdit.time()) - self.manager.dataset_start).total_seconds())
+        self.segment_date_slider.setValue((Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                                                 self.segment_date_dateTimeEdit.time()) - self.manager.dataset_start).total_seconds())
 
-    def updateSegmentDateEditor(self):
+        self.segment_date_slider.sliderReleased.connect(self.update_segment_date_editor)
+
+    def update_segment_date_editor(self, secondary_trigger=False):
         """ Update the segment date editor after the slider is changed.
         :return: None.
         """
+        self.segment_date_dateTimeEdit.dateTimeChanged.disconnect(self.update_segment_date_slider)
+        self.segment_date_dateTimeEdit.dateTimeChanged.disconnect(self.update_plot)
+
         delta = self.segment_length_doubleSpinBox.value() / 2
         delta = datetime.timedelta(seconds=delta)
-        self.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(
-            self.manager.dataset_start + delta + datetime.timedelta(seconds=self.segment_date_slider.value())))
+        old_date = Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                         self.segment_date_dateTimeEdit.time())
+        new_date = self.manager.dataset_start + delta + datetime.timedelta(seconds=self.segment_date_slider.value())
+        self.spectralViewer.notify_delta(new_date - old_date, self)
+        self.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(new_date))
 
-    def updateDatesBounds(self):
+        self.segment_date_dateTimeEdit.dateTimeChanged.connect(self.update_segment_date_slider)
+        self.segment_date_dateTimeEdit.dateTimeChanged.connect(self.update_plot)
+
+    def update_date_bounds(self):
         """ Update the current min and max allowed segment date given current segment length (to avoid getting out of
         the dataset).
         :return: None.
@@ -202,26 +216,25 @@ class SpectralView(QtWidgets.QWidget):
         delta = datetime.timedelta(seconds=delta)
         start = self.manager.dataset_start + delta
         end = self.manager.dataset_end - delta
-        self.segment_date_dateTimeEdit.setMinimumDateTime(DatetimeToQdatetime(start))
-        self.segment_date_dateTimeEdit.setMaximumDateTime(DatetimeToQdatetime(end))
+        self.segment_date_dateTimeEdit.setMinimumDateTime(datetime_to_Qdatetime(start))
+        self.segment_date_dateTimeEdit.setMaximumDateTime(datetime_to_Qdatetime(end))
         self.segment_date_slider.setMinimum(0)
         self.segment_date_slider.setMaximum((end - start).total_seconds())
 
-    def _updateData(self):
+    def _update_data(self):
         """ Update the spectrogram value.
         :return: None.
         """
         delta = datetime.timedelta(seconds=self.segment_length_doubleSpinBox.value() / 2)
-        start, end = self.getTimeBounds()
+        start, end = self.get_time_bounds()
         # get the spectro from the extractor to be able to show it
         data = self.manager.get_segment(start, end)
-        (f, t, spectro) = signal.spectrogram(data, self.manager.sampling_f, nperseg=256, noverlap=128)
-        spectro = 10*np.log10(spectro).astype(np.float32)[::-1].copy()
+        (f, t, spectro) = make_spectrogram(data, self.manager.sampling_f, t_res=0.5342, f_res=0.9375, return_bins=True, normalize=True, vmin=60, vmax=120)
 
         # label the time axis from -delta to +delta
         extent = [min(t) - delta.total_seconds(), max(t) - delta.total_seconds(), min(f), max(f)]
         self.mpl.axes.cla()
-        self.mpl.axes.imshow(spectro, aspect="auto", extent=extent, vmin=0, vmax=255, cmap="jet")
+        self.mpl.axes.imshow(spectro, aspect="auto", extent=extent, vmin=0, vmax=1, cmap="jet")
         self.mpl.axes.set_xlabel('t (s)')
         self.mpl.axes.set_ylabel('f (Hz)')
 
@@ -231,38 +244,38 @@ class SpectralView(QtWidgets.QWidget):
         """
         self.mpl.draw()
 
-    def updatePlot(self):
+    def update_plot(self):
         """ Update the spectrogram and then the plot widget.
         :return: None.
         """
-        self._updateData()
+        self._update_data()
         self._draw()
 
-    def onclickGraph(self, click):
+    def on_click(self, click):
         """ Handles click operation to enable point and click move.
         :param click: Object giving details about the click.
         :return: None.
         """
         if click.xdata is not None:
-            segment_center = QdatetimeToDatetime(self.segment_date_dateTimeEdit.date(),
-                                                 self.segment_date_dateTimeEdit.time())
+            segment_center = Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                                   self.segment_date_dateTimeEdit.time())
             segment_center += datetime.timedelta(seconds=click.xdata)  # move to the click location
-            self.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(segment_center))
+            self.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(segment_center))
 
-    def onkeyGraph(self, key):
+    def on_key(self, key):
         """ Handles key press operations by passing the event to the parent spectral_viewer.
         :param key: Object giving details about the key press.
         :return: None.
         """
-        self.spectralViewer.onkeyGraph_spectral_view(self, key)
+        self.spectralViewer.on_key(self, key)
 
-    def onkeyGraph_local(self, key):
+    def on_key_local(self, key):
         """ Handles key press operations to enable various shortcuts, only acting on the current spectral_view.
         :param key: Object giving details about the key press.
         :return: None.
         """
-        segment_center = QdatetimeToDatetime(self.segment_date_dateTimeEdit.date(),
-                                             self.segment_date_dateTimeEdit.time())
+        segment_center = Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                               self.segment_date_dateTimeEdit.time())
         delta = datetime.timedelta(seconds=self.segment_length_doubleSpinBox.value() / 2)
 
         if key.key == 'right':
@@ -296,7 +309,7 @@ class SpectralView(QtWidgets.QWidget):
             self.freq_range[1] += d
 
         elif key.key == "down":
-            # decrase min and max allowed frequency, respecting the limit of 0
+            # decrease min and max allowed frequency, respecting the limit of 0
             d = min(self.freq_range[0],
                     0.1 * self.manager.sampling_f / 2)
             self.freq_range[0] -= d
@@ -319,21 +332,21 @@ class SpectralView(QtWidgets.QWidget):
 
         elif key.key == "ctrl+enter":
             # put all the SpectralView widgets of the parent window to the same configuration
-            segment_center = QdatetimeToDatetime(self.segment_date_dateTimeEdit.date(),
-                                                 self.segment_date_dateTimeEdit.time())
+            segment_center = Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                                   self.segment_date_dateTimeEdit.time())
             for spectralview in self.spectralViewer.SpectralViews:
                 spectralview.freq_range = self.freq_range.copy()
-                spectralview.changeDate(segment_center)
-                spectralview.changeSegmentLength(self.segment_length_doubleSpinBox.value())
+                spectralview.change_date(segment_center)
+                spectralview.change_segment_length(self.segment_length_doubleSpinBox.value())
         else:
             return
 
         # update widgets
-        self.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(segment_center))
+        self.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(segment_center))
         self.segment_length_doubleSpinBox.setValue(delta.total_seconds() * 2)
-        self.updatePlot()
+        self.update_plot()
 
-    def changeDate(self, new_date):
+    def change_date(self, new_date):
         """ Change the date the widget focuses on to a new one.
         :param new_date: The new datetime to focus on.
         :return: None.
@@ -342,9 +355,9 @@ class SpectralView(QtWidgets.QWidget):
         new_date = new_date + datetime.timedelta(seconds=round(new_date.microsecond / 10 ** 6))
         new_date = new_date.replace(microsecond=0)
         # simply update the segment date editor which will cascade the change
-        self.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(new_date))
+        self.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(new_date))
 
-    def changeSegmentLength(self, new_length):
+    def change_segment_length(self, new_length):
         """ Change the current segment length.
         :param new_length: The new length, in seconds.
         :return: None.
@@ -352,18 +365,18 @@ class SpectralView(QtWidgets.QWidget):
         # simply update the segment length editor which will cascade the change
         self.segment_length_doubleSpinBox.setValue(new_length)
 
-    def getTimeBounds(self):
+    def get_time_bounds(self):
         """ Get the current time bounds of the tool and return them as a tuple.
         :return: A tuple (start, end) of datetimes.
         """
-        segment_center = QdatetimeToDatetime(self.segment_date_dateTimeEdit.date(),
-                                             self.segment_date_dateTimeEdit.time())
+        segment_center = Qdatetime_to_datetime(self.segment_date_dateTimeEdit.date(),
+                                               self.segment_date_dateTimeEdit.time())
         delta = datetime.timedelta(seconds=self.segment_length_doubleSpinBox.value() / 2)
 
         return segment_center - delta, segment_center + delta
 
 
-def QdatetimeToDatetime(qdate, qtime):
+def Qdatetime_to_datetime(qdate, qtime):
     """ Utility function to convert PySide dates and times to Python datetimes.
     :param qdate: A PySide date (Y,M,D).
     :param qtime: A Pyside time (h,m,s).
@@ -373,7 +386,7 @@ def QdatetimeToDatetime(qdate, qtime):
                              qtime.hour(), qtime.minute(), qtime.second())
 
 
-def DatetimeToQdatetime(datetime, day_offset=0):
+def datetime_to_Qdatetime(datetime, day_offset=0):
     """ Utility function to convert Python datetimes to PySide datetimes.
     :param datetime: A Python datetime.
     :return: A PySide datetime.

src/GUI/widgets/spectral_view_augmented.py
@@ -1,11 +1,13 @@
 import numpy as np
 import torch
-from scipy import signal
+from torchvision.transforms import Resize
 from scipy.signal import find_peaks
 from skimage.transform import resize
 
 from GUI.widgets.mpl_canvas import MplCanvas
 from GUI.widgets.spectral_view import SpectralView, MIN_SEGMENT_DURATION_S
+from utils.physics.signal.make_spectrogram import make_spectrogram
+
 
 class SpectralViewTissnet(SpectralView):
     """ Spectral view enabling, with shift+enter, to apply TiSSNet on the current spectrogram.
@@ -20,22 +22,20 @@ class SpectralViewTissnet(SpectralView):
         :param kwargs: Supplementary key arguments for the widget as a PySide widget.
         """
         super().__init__(SpectralViewer, station, date, delta_view_s, *args, **kwargs)
-        self.initMpl()
+        self.init_mpl()
         self.mpl_layout.addWidget(self.mpl_models)
 
-    def onkeyGraph(self, key):
+    def on_key(self, key):
         """ Checks if the TiSSNet shortcut has been pressed.
         :param key: The key pressed.
         :return: None.
         """
         if key.key == "shift+enter":
-            self.processTissnet()
-        elif key.key == "alt+enter":
-            self.spectralViewer.associate(self)
+            self.process_tissnet()
         else:
-            super().onkeyGraph(key)
+            super().on_key(key)
 
-    def initMpl(self):
+    def init_mpl(self):
         """ Initialize the Matplotlib widget.
         :return: None
         """
@@ -45,7 +45,7 @@ class SpectralViewTissnet(SpectralView):
         self.mpl_models.setFixedHeight(40)
         self.mpl_models.setVisible(False)
 
-    def processTissnet(self):
+    def process_tissnet(self):
         """ Apply TiSSNet on the current spectrogram and adds the result below it with a colormap.
         :return: None.
         """
@@ -53,21 +53,14 @@ class SpectralViewTissnet(SpectralView):
             print("Trying to use TiSSNet but it has not been loaded")
             return
 
-        start, end = self.getTimeBounds()
+        start, end = self.get_time_bounds()
         data = self.manager.get_segment(start, end)
-        (f, t, spectro) = signal.spectrogram(data, self.manager.sampling_f, nperseg=256, noverlap=128)
-        spectro = 10 * np.log10(spectro).astype(np.float32)[::-1].copy()
-        # normalization
-        spectro = resize(spectro, (1, 128, spectro.shape[2]))[np.newaxis, :, :]
-        spectro[spectro < -35] = -35
-        spectro[spectro > 140] = 140
-        spectro = (spectro + 35) / (35 + 140)
-        spectro = torch.from_numpy(spectro)
+        spectrogram = make_spectrogram(data, self.manager.sampling_f, t_res=0.5342, f_res=0.9375, return_bins=False,
+                                       normalize=True, vmin=-35, vmax=140).astype(np.float32)
+        spectrogram = spectrogram[np.newaxis, :, :]
+        input_data = Resize((128, spectrogram.shape[-1]))(torch.from_numpy(spectrogram))  # resize data
         with torch.no_grad():
-            res = self.spectralViewer.detection_model(spectro).numpy()
-
-        peaks = find_peaks(res, height=0.1, distance=10)
-        print(peaks)
+            res = self.spectralViewer.detection_model(input_data).numpy()
 
         self.showModelResult(res)
 
@@ -77,7 +70,7 @@ class SpectralViewTissnet(SpectralView):
         :return: None.
         """
         self.mpl_layout.removeWidget(self.mpl_models)
-        self.initMpl()
+        self.init_mpl()
         self.mpl_layout.addWidget(self.mpl_models)
 
         self.mpl_models.axes.imshow(to_show.reshape((1, -1)), aspect="auto", vmin=0, vmax=1)

src/GUI/windows/spectral_viewer.py
@@ -12,21 +12,22 @@ from PySide6.QtGui import (QAction)
 from PySide6.QtWidgets import (QMainWindow, QToolBar)
 from skimage.transform import resize
 
-from GUI.widgets.spectral_view import SpectralView, QdatetimeToDatetime, DatetimeToQdatetime
+from GUI.widgets.spectral_view import SpectralView, Qdatetime_to_datetime, datetime_to_Qdatetime
 from GUI.widgets.spectral_view_augmented import SpectralViewTissnet
 from utils.data_reading.sound_data.station import StationsCatalog, Station
 from utils.physics.sound_model.sound_model import HomogeneousSoundModel
-from utils.detection.TiSSNet import TiSSNet  # seems useless but enables PyTorch to retrieve the model definition
+from utils.detection.TiSSNet import TiSSNet
 
 MIN_SPECTRAL_VIEW_HEIGHT = 400
 DELTA_VIEW_S = 200
 class SpectralViewerWindow(QMainWindow):
     """ Window containing several SpectralView widgets and enabling to import them one by one or in group.
     """
-    def __init__(self, database_yaml, tissnet_checkpoint=None):
+    def __init__(self, database_yaml, tissnet_checkpoint=None, events_path=None):
         """ Constructor initializing the window and setting its visual appearance.
         :param database_yaml: YAML file containing information about the available stations.
         :param tissnet_checkpoint: Checkpoint of TiSSNet model in case we want to try detection.
+        :param events_path: Path of a yaml file describing some events.
         """
         # TiSSNet
         self.detection_model = None
@@ -34,6 +35,7 @@ class SpectralViewerWindow(QMainWindow):
             self.detection_model = TiSSNet()
             self.detection_model.load_state_dict(torch.load(tissnet_checkpoint))
 
+        self.events_path = events_path
 
         super().__init__()
         self.sound_model = HomogeneousSoundModel()
@@ -74,7 +76,7 @@ class SpectralViewerWindow(QMainWindow):
         # add a toolbar
         self.toolBar = QToolBar(self.centralWidget)
         self.addToolBar(Qt.TopToolBarArea, self.toolBar)
-        self.toolBar.actionTriggered.connect(self.toolbarAction)
+        self.toolBar.actionTriggered.connect(self.toolbar_action)
         # add a button to add sound folders
         self.action_add_a_sound_folder = QAction(self)
         self.action_add_a_sound_folder_text = "Add a sound folder"
@@ -121,7 +123,7 @@ class SpectralViewerWindow(QMainWindow):
         for view in self.SpectralViews:  # resize other spectral views if needed
             view.setFixedHeight(max(self.height() * 0.9 // len(self.SpectralViews), MIN_SPECTRAL_VIEW_HEIGHT))
 
-    def _addDir(self, directory, depth=0, max_depth=3):
+    def add_dir(self, directory, depth=0, max_depth=3):
         """ Given a directory, add a SpectralView if it is a sound files directory, else recursively look for sound
         files.
         :param directory: The directory to consider.
@@ -148,9 +150,9 @@ class SpectralViewerWindow(QMainWindow):
                 # check we didn't reach the max depth of recursive calls
                 if depth < max_depth:
                     for subdir in glob2.glob(f'{directory}/*/'):
-                        self._addDir(subdir, depth=depth+1, max_depth=max_depth)
+                        self.add_dir(subdir, depth=depth + 1, max_depth=max_depth)
 
-    def onkeyGraph_spectral_view(self, spectral_view, key):
+    def on_key(self, spectral_view, key):
         """ Method called by a spectral_view child when a key is pressed on it.
         :param spectral_view: The spectral_view focused by the user.
         :param key: Object providing information about the key pressed.
@@ -159,20 +161,20 @@ class SpectralViewerWindow(QMainWindow):
         # in case the broadcast checkbox is checked, we broadcast if possible the shortcut to all spectral view
         if self.broadcast_checkbox.isChecked() and 'enter' not in key.key:
             for spectral_view in self.SpectralViews:
-                spectral_view.onkeyGraph_local(key)
+                spectral_view.on_key_local(key)
         else:
-            spectral_view.onkeyGraph_local(key)
+            spectral_view.on_key_local(key)
 
     def notify_delta(self, delta, spectral_view):
         if self.broadcast_checkbox.isChecked():
             for spectral_view_ in self.SpectralViews:
                 if spectral_view_ != spectral_view:
-                    segment_center = QdatetimeToDatetime(spectral_view_.segment_date_dateTimeEdit.date(),
-                                                         spectral_view_.segment_date_dateTimeEdit.time())
+                    segment_center = Qdatetime_to_datetime(spectral_view_.segment_date_dateTimeEdit.date(),
+                                                           spectral_view_.segment_date_dateTimeEdit.time())
                     segment_center += delta
                     # we temporarily disable the broadcast checkbox to avoid recursive calls
                     self.broadcast_checkbox.setChecked(False)
-                    spectral_view_.segment_date_dateTimeEdit.setDateTime(DatetimeToQdatetime(segment_center))
+                    spectral_view_.segment_date_dateTimeEdit.setDateTime(datetime_to_Qdatetime(segment_center))
                     self.broadcast_checkbox.setChecked(True)
 
     def clear_spectral_views(self):
@@ -184,7 +186,7 @@ class SpectralViewerWindow(QMainWindow):
         for spectral_view in self.SpectralViews.copy():
             self.close_spectral_view(spectral_view)
 
-    def toolbarAction(self, qAction):
+    def toolbar_action(self, qAction):
         """ Method triggered by a click on a toolbar button.
         :param qAction: Action variable passed by PySide when a toolbar button is clicked.
         :return: None.
@@ -193,13 +195,13 @@ class SpectralViewerWindow(QMainWindow):
         if qAction.text() == self.action_add_a_sound_folder_text:
             # open a files browser dialog to enable the user to add new SpectralView widgets to the window
             directory = QFileDialog.getExistingDirectory(self, 'directory location')  # files browser
-            self._addDir(directory)  # inspection of the selected directory
+            self.add_dir(directory)  # inspection of the selected directory
         elif qAction.text() == self.action_clear_text:
             # clear spectral views
             self.clear_spectral_views()
         elif qAction.text() == self.action_pick_event_text:
             # open a selection dialog to choose an event to visualize
-            with open("../data/GUI/events.yaml", "r") as f:
+            with open(self.events_path, "r") as f:
                 events = yaml.load(f, Loader=yaml.BaseLoader)
             self.clear_spectral_views()
             to_display = list(events.keys())
@@ -239,33 +241,11 @@ class SpectralViewerWindow(QMainWindow):
         if None in sensors_positions.reshape(-1):
             self.srcEstimateLabel.setText("Station position is missing!")
             return
-        detection_times = np.array([QdatetimeToDatetime(s.segment_date_dateTimeEdit.date(),
-                                             s.segment_date_dateTimeEdit.time()) for s in self.SpectralViews if s.focused])
+        detection_times = np.array([Qdatetime_to_datetime(s.segment_date_dateTimeEdit.date(),
+                                                          s.segment_date_dateTimeEdit.time()) for s in self.SpectralViews if s.focused])
         src = self.sound_model.localize_common_source(sensors_positions, detection_times)
         time = detection_times[0] + np.mean([(detection_times[i] -
                         datetime.timedelta(seconds=self.sound_model.get_sound_travel_time(src.x[1:], sensors_positions[i], detection_times[0]))) - detection_times[0]
                          for i in range(len(sensors_positions))])
         self.srcEstimateLabel.setText(f'Location estimate : {[float(f"{v:.2f}") for v in src.x]} at '
                                       f'{time.strftime("%Y%m%d_%H%M%S")} (cost {src.cost:.2f})')
-
-    def associate(self, ref_spectral_view):
-        ''' Run the embedder on all stations, taking as ref the selected one.
-        :param ref_spectral_view: The selected SpectralView (calling this method).
-        :return: None.
-        '''
-        time_res, half_focus = 0.5, 16
-        embedding_ref = ref_spectral_view.processEmbedder(margin=datetime.timedelta(seconds=10))
-        embedding_ref = embedding_ref[:, embedding_ref.shape[1]//2-half_focus:embedding_ref.shape[1]//2+half_focus]
-        embedding_ref = resize(embedding_ref, (16, 1+2*half_focus))
-        for spectral_view in self.SpectralViews:
-            if spectral_view != ref_spectral_view:
-                embedding = ref_spectral_view.processEmbedder(margin=datetime.timedelta(seconds=half_focus), keep_margin=True)
-                embedding = resize(embedding, (16, embedding.shape[1]))
-
-                half_duration = int((spectral_view.segment_length_doubleSpinBox.value() / 2) / time_res)
-                embedding = [embedding[:, embedding.shape[1]//2 + i - half_focus:embedding.shape[1]//2 + i + half_focus + 1] for i in
-                         range(-half_duration, half_duration + 1)]
-                embedding = np.array(embedding)
-                diff = np.sqrt(np.sum((embedding_ref[np.newaxis, :, :] - embedding) ** 2, axis=(1, 2)))
-                diff = diff / np.sqrt(len(diff) * 16 * 2)  # normalisation by max theoretical value
-                spectral_view.showModelResult(diff)