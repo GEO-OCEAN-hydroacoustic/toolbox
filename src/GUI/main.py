88aa96ddff41b5ae8ba42ca061d4284b731fe0c5
Pierre-Yves <pierre-yves.raumer@univ-brest.fr>
2025-01-15 15:05:21 +0400

    adding localization logging to the GUI, reducing SpectralViewers height and fixing station recognization issue

--- src/GUI/main.py
+++ src/GUI/main.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-
+import matplotlib
 from PySide6.QtWidgets import QApplication
 
 from GUI.windows.spectral_viewer import SpectralViewerWindow
@@ -9,16 +9,20 @@ if __name__ == "__main__":
     # main to call in order to launch the dataset exploration tool.
     app = QApplication(sys.argv)
     app.setStyleSheet(
-        "QLabel{font-size: 16pt;} QDateTimeEdit{font-size: 16pt;} QPushButton{font-size: 20pt;} QDoubleSpinBox{font-size: 16pt;}")
+        "QLabel{font-size: 10pt;} QDateTimeEdit{font-size: 10pt;} QPushButton{font-size: 10pt;} QDoubleSpinBox{font-size: 10pt;}")
+    matplotlib.rcParams.update({'font.size': 10})
 
     # dataset yaml to get more information about the stations ; the file can be empty
     datasets_yaml = "../../data/demo/dataset.yaml"
+    datasets_yaml = "/home/plerolland/Bureau/dataset.yaml"
 
     tissnet_checkpoint = "../../data/models/TiSSNet/torch_save"  # TiSSNet weights for demonstration purpose ; optional
     if not Path(tissnet_checkpoint).exists():
         tissnet_checkpoint = None
 
-    window = SpectralViewerWindow(datasets_yaml, tissnet_checkpoint=tissnet_checkpoint, events_path="../../data/GUI/events.yaml")
+    window = SpectralViewerWindow(datasets_yaml, tissnet_checkpoint=tissnet_checkpoint,
+                                  events_path="../../data/GUI/events.yaml",
+                                  loc_res_path="../../data/GUI/localizations.csv")
     window.show()
 
     sys.exit(app.exec())
